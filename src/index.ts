a38603dedf830b9833cddab632aee3492d608f7e
Andy Barron <AndrewLBarron@gmail.com>
2020-04-08 03:48:14 -0700

    try/catch message handler

--- src/index.ts
+++ src/index.ts
@@ -19,26 +19,32 @@ async function main() {
   })
 
   client.on('message', async (message) => {
-    logger.info(message.content)
-    const { content } = message
-    const prefix = '!meme'
-    if (!message.author.bot && content.split(/\s+/)[0] === prefix) {
-      const argText = content.substring(prefix.length).replace(/^\s+/, '')
-      const args = argText.split(/(?:[^\\])\|/).map((arg) => arg.trim())
-      logger.info({ args })
-      if (args.length < 2) {
-        message.channel.send([
-          'Command format:',
-          '`!meme template name | caption 1`',
-          '`!meme template name | caption 1 | caption 2` (etc)',
-          'Example:',
-          '`!meme is this a pigeon | me | writing discord bots | is this a hobby?`',
-        ])
-        return
+    try {
+      const { content } = message
+      const prefix = '!meme'
+      if (!message.author.bot && content.split(/\s+/)[0] === prefix) {
+        const argText = content.substring(prefix.length).replace(/^\s+/, '')
+        const args = argText.split(/(?:[^\\])\|/).map((arg) => arg.trim())
+        logger.info({ args })
+        if (args.length < 2) {
+          message.channel.send([
+            'Command format:',
+            '`!meme template name | caption 1`',
+            '`!meme template name | caption 1 | caption 2` (etc)',
+            'Example:',
+            '`!meme is this a pigeon | me | writing discord bots | is this a hobby?`',
+          ])
+          return
+        }
+        const [name, ...captions] = args
+        const result = await manager.createMeme({ name, captions })
+        await message.channel.send(result.imageUrl)
       }
-      const [name, ...captions] = args
-      const result = await manager.createMeme({ name, captions })
-      await message.channel.send(result.imageUrl)
+    } catch (err) {
+      logger.error(
+        { err, messageContent: message.content },
+        'Error handling message',
+      )
     }
   })
 
