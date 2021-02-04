25a7024a9af51016a3e5bbcd2137c0759884985e
Andy Barron <andrewlbarron@gmail.com>
2021-02-04 18:19:22 +0000

    fix log level crash

--- src/logger.ts
+++ src/logger.ts
@@ -2,7 +2,7 @@ import pino from 'pino'
 import { DEV_MODE } from './config'
 
 export const logger = pino({
-  level: process.env.LOG_LEVEL,
+  level: process.env.LOG_LEVEL || 'info',
   prettyPrint: DEV_MODE && {
     levelFirst: true,
     translateTime: 'SYS:standard',
